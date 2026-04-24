62759c1e5e2f8935cb130828fde6debf84a9fa9d
Moneer Mousa <mmousa10@smail.ucas.edu.ps>
2026-04-24 16:02:26 +0300

    edit all routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +20,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+})->name('home');
+
+Route::middleware(['auth', 'isAdmin'])->group(function () { // only admin users
+    Route::resource('products', ProductController::class)->except(['index', 'show']);
+    Route::resource('users', UserController::class);
+});
+
+
+Route::middleware(['auth'])->group(function () { // registered users
+    Route::resource('orders', OrderController::class);
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+
+// all
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+
+
+
+// guest only
+Route::middleware(['guest'])->group(function () {
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.store');
+
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.store');
+});
+
+Route::middleware(['auth', 'isAdmin'])->group(function () { // only admin users
+    Route::resource('products', ProductController::class)->except(['index', 'show']);
+    Route::resource('users', UserController::class);
 });
